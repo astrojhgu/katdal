a7083e53e7c74bdb651d84f6a75b262ac9b2fd86
GU Junhua <jhgu@nao.cas.cn>
2017-02-01 18:36:10 +0800

    修改：     setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 ################################################################################
 # Copyright (c) 2011-2016, National Research Foundation (Square Kilometre Array)
